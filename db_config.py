dd8b071e2afbc376b5cbb6c6ff35a51bf2ab57df
Yory Sreykhuoch <yorysreykhuoch9@gmail.com>
2025-05-18 11:38:33 +0700

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='19062004@khuoch',
+        password='yoru passsword',
         database='MySystem'
     )
